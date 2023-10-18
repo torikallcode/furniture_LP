922bc6df20c0456350fff8f39d3be905e98b65b0
torikallcode <torikallcode@gmail.com>
2023-10-18 21:05:44 +0800

    commit furnitur

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        utama: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        putih: "#FFF",
+        hitam: "#564E4E",
+        secondary: "#F79489",
+      },
+    },
+  },
+  plugins: [],
+};
+
